924ad68098667171e1ea431607ae12efe830cc95
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-04-01 16:29:56 -0700

    Return label for Todo item

--- src/Todo.js
+++ src/Todo.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function Todo({ todo }) {
-  return <div>{todo}</div>;
+  return (
+    <label>
+      <input type="checkbox" checked={todo.complete} />
+      <div>{todo.name}</div>
+    </label>
+  );
 }
