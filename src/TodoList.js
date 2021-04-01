898d94f305a615bc9d3c801a9061d24e308b1bd3
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-04-01 16:22:32 -0700

    Import todo and map state to generate new todos

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,8 @@
 import React from "react";
+import Todo from "./Todo";
 
-export default function TodoList() {
-  return (
-    <div>
-      <p>Hello Todo</p>
-    </div>
-  );
+export default function TodoList({ todos }) {
+  return todos.map((todo) => {
+    return <Todo key={todo} todo={todo} />;
+  });
 }
