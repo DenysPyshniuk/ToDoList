8cb0f6e74db59bd58f6cbaa668e71db8f275c1a7
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-04-02 12:38:28 -0700

    Add style for todo list

--- src/Todo.js
+++ src/Todo.js
@@ -1,13 +1,15 @@
 import React from "react";
+import "./App.css";
 
 export default function Todo({ todo, toggleTodo }) {
   function handleTodoClick() {
     toggleTodo(todo.id);
   }
   return (
-    <div>
-      <label>
+    <div id="todo">
+      <label class="checkbox">
         <input
+          class="checkbox"
           type="checkbox"
           checked={todo.complete}
           onChange={handleTodoClick}
